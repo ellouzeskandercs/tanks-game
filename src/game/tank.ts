a0cd4df6ca5203d9f3ada7dc5609ba6df4e87c49
Skander <skander.ellouze@student-cs.fr>
2021-06-10 22:11:03 +0200

    implement obstacles collision

--- src/game/tank.ts
+++ src/game/tank.ts
@@ -1,7 +1,17 @@
 import Phaser from 'phaser';
 import { Angle, ANGLE_UNIT } from './angle'
+import { Rectangle } from './rectangle';
+import { Point, Vector } from './point';
 
 export class Tank {
+    private static _getObjectTrigoCoordinates(point: Point): Point{
+        /* To do move this to other helper */
+        return ({
+            x: point.y,
+            y: point.x
+        })
+    }
+
     private _containerObject: Phaser.GameObjects.Container;
     private _vehicleObject: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
     private _cannonObject: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
@@ -9,7 +19,7 @@ export class Tank {
     private _velocity: number = 160;
     private _angularVelocity: number = 80;
 
-    constructor(scene: Phaser.Scene, x: number = 0, y:number = 0, obstacles: Phaser.Physics.Arcade.StaticGroup) {
+    constructor(scene: Phaser.Scene, x: number = 0, y:number = 0) {
         this._scene = scene;
 
         this._containerObject = this._scene.add.container(x, y);
@@ -26,14 +36,51 @@ export class Tank {
 
         this._containerObject.addAt(this._vehicleObject, 0);
         this._containerObject.addAt(this._cannonObject, 1);
-
-        // this._scene.physics.add.overlap(this._containerObject, obstacles, (a, b) => console.log(a,b), this.isColliding)
     }
 
-    // private isColliding(a: any, b: any) {
-    //     console.log(a,b)
-    //     return false
-    // }
+    private _getObjectAsRectangle(object: Phaser.Physics.Arcade.Sprite): Rectangle {
+        const rotation = { 
+            origin: Tank._getObjectTrigoCoordinates({ 
+                x: object.body.x + object.originX * object.width,
+                y: object.body.y + object.originY * object.height
+            }),
+            angle: new Angle(- object.angle, ANGLE_UNIT.DEGREES)
+        }
+        const rectangle = new Rectangle(Tank._getObjectTrigoCoordinates(object.body.position), object.body.width, object.body.height, rotation);
+        return rectangle;
+    }
+
+    public setObstacleCollider(object: Phaser.GameObjects.GameObject | Phaser.GameObjects.GameObject[] | Phaser.GameObjects.Group | Phaser.GameObjects.Group[]) {
+        this._scene.physics.add.overlap(this._containerObject, object, (object1, object2) => this.histTest(object1, object2))
+    }
+
+    public histTest(object1: Phaser.Types.Physics.Arcade.GameObjectWithBody, object2: Phaser.Types.Physics.Arcade.GameObjectWithBody): void {
+
+        const cannonRectangle = this._getObjectAsRectangle(this._cannonObject);
+        const vehicleRectangle = this._getObjectAsRectangle(this._vehicleObject);
+        const obstacleRectangle = new Rectangle(Tank._getObjectTrigoCoordinates(object2.body.position), object2.body.width, object2.body.height)
+
+        const cannonHitsObstacle = cannonRectangle.getBounds().reduce((acc:boolean, point: Point) => obstacleRectangle.isPointInside(point) || acc, false);
+        const vehicleHitsObstacle = vehicleRectangle.getBounds().reduce((acc:boolean, point: Point) => obstacleRectangle.isPointInside(point) || acc, false);
+        const obstacleHitsVehicle = obstacleRectangle.getBounds().reduce((acc:boolean, point: Point) => vehicleRectangle.isPointInside(point) || acc, false);
+        const obstacleHitsCannon = obstacleRectangle.getBounds().reduce((acc:boolean, point: Point) => cannonRectangle.isPointInside(point) || acc, false);
+        
+        if(cannonHitsObstacle || obstacleHitsCannon || vehicleHitsObstacle || obstacleHitsVehicle){
+            console.log(object2.body)
+            const pushDirection: Vector = {
+                x: this._containerObject.x - object2.body.center.x,
+                y: this._containerObject.y - object2.body.center.y
+            }
+            this.pushTank(pushDirection, 10);
+        }
+    }
+    
+    public pushTank(directionVector: Vector, distance: number): void {
+        const vehiculeAngle: Angle = new Angle(this._vehicleObject.angle, ANGLE_UNIT.DEGREES);
+
+        this._containerObject.x = this._containerObject.x + Math.sign(directionVector.x) * Math.abs(vehiculeAngle.cos()) * distance;
+        this._containerObject.y = this._containerObject.y + Math.sign(directionVector.y) * Math.abs(vehiculeAngle.sin()) * distance;
+    }
 
     public pointCannonToPoint(x: number, y: number) {
         this._cannonObject.rotation = Phaser.Math.Angle.Between(this._containerObject.x, this._containerObject.y, x, y);
@@ -42,11 +89,8 @@ export class Tank {
     public moveToDirection(directionAngle: Angle) {
         const vehiculeAngle: Angle = new Angle(this._vehicleObject.angle, ANGLE_UNIT.DEGREES);
         const angleDiff: Angle = Angle.sum(vehiculeAngle, Angle.multiply(directionAngle, -1))
-        console.log('anglediff',vehiculeAngle, Angle.multiply(directionAngle, -1), angleDiff, angleDiff.sin(), angleDiff.cos());
         if (angleDiff.sin()**2 < 0.01 && angleDiff.cos()**2 > 0.81) {
             this._vehicleObject.body.setAngularVelocity(0);
-            // console.log(directionAngle.cos());
-            // console.log(directionAngle.sin());
             (this._containerObject.body as Phaser.Physics.Arcade.Body).setVelocity(directionAngle.cos() * this._velocity, directionAngle.sin() * this._velocity);
         } else {
             this._vehicleObject.body.setAngularVelocity(angleDiff.sin() * angleDiff.cos() < 0 ? this._angularVelocity : -this._angularVelocity);
@@ -58,5 +102,4 @@ export class Tank {
         this._vehicleObject.body.setAngularVelocity(0);
         (this._containerObject.body as Phaser.Physics.Arcade.Body).setVelocity(0, 0);
     }
-
 }
