389b3b40a84d49c984b876342bfabb9e1ff9c6a1
Skander <skander.ellouze@student-cs.fr>
2021-07-05 21:34:10 +0200

    better design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import logo from './logo.svg';
-import './App.css';
-import { Welcome } from './game/game.tsx'
+import './App.scss';
+import { Game } from './game/game.tsx'
 
 
 function App() {
   return (
-    <Welcome>
+    <div className="app-container">
+      <header class="w-75">
+        <h3 class="float-md-start mb-0">Tanks Game</h3>
+        <nav class="nav nav-masthead justify-content-center float-md-end">
+          <a class="nav-link active" aria-current="page" href="#">Play</a>
+          <a class="nav-link" href="#">Best scores</a>
+          <a class="nav-link" href="#">Forum</a>
+        </nav>
+      </header>
+      <Game></Game>
+    </div>
 
-    </Welcome>
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
--- src/game/constants.ts
+++ src/game/constants.ts
@@ -7,35 +7,35 @@ export interface assetImage {
 
 const tankImage: assetImage = {
     name: 'tank',
-    path: 'assets/tank.png',
+    path: 'tanks-game/assets/tank.png',
     width: 40,
     height: 30
 }
 
 const obstacleImage: assetImage = {
     name: 'obstacle',
-    path: 'assets/obstacle.png',
+    path: '/tanks-game/assets/obstacle.png',
     width: 274,
     height: 82
 }
 
 const cannonImage: assetImage = {
     name: 'cannon',
-    path: 'assets/cannon.png',
+    path: '/tanks-game/assets/cannon.png',
     width: 40,
     height: 10
 }
 
 const backgroundImage: assetImage = {
     name: 'background',
-    path: 'assets/background.png',
+    path: '/tanks-game/assets/background.png',
     width: 800,
     height: 500
 }
 
 const bulletImage: assetImage = {
     name: 'bullet',
-    path: 'assets/bullet.png',
+    path: '/tanks-game/assets/bullet.png',
     width: 15,
     height: 8
 }
